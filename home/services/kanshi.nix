ed83bd9c08cea346bb2ff436b433f54719253083
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2024-08-23 14:18:03 +0300

    use kanshi.settings instead of the depricated kanshi.profiles

--- home/services/kanshi.nix
+++ home/services/kanshi.nix
@@ -2,20 +2,21 @@
 {
   services.kanshi = {
     enable = true;
-    profiles = {
-      main = {
-        outputs = [
-	  {
-	    criteria = "HDMI-A-1";
-	    # scale = 1.4;
-	    position = "0,0";
-	  }
-	  {
-	    criteria = "eDP-1";
-	    position = "1980,0";
-	  }
-	];
-      };
-    };
+    settings = [
+      {
+        profile.name = "main";
+        profile.outputs = [
+      	  {
+      	    criteria = "HDMI-A-1";
+      	    # scale = 1.4;
+      	    position = "0,0";
+      	  }
+      	  {
+      	    criteria = "eDP-1";
+      	    position = "1980,0";
+      	  }
+      	];
+      }
+    ];
   };
 }
