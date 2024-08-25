4052446682dc84c6329c4096c3d5c42658c0ce84
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2024-08-25 14:28:43 +0300

    improve helix configurations

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.helix = {
     enable = true;
     defaultEditor = true;
@@ -11,7 +12,10 @@
         space.space = "file_picker";
         space.w = ":w";
         space.q = ":q";
-        esc = [ "collapse_selection" "keep_primary_selection" ];
+        esc = [
+          "collapse_selection"
+          "keep_primary_selection"
+        ];
       };
       keys.insert = {
         j.k = "normal_mode";
@@ -24,30 +28,75 @@
       };
     };
     languages = {
+      language =
+        let
+          lang = name: formatter-command: {
+            name = name;
+            formatter = {
+              command = formatter-command;
+            };
+            auto-format = true;
+            indent = {
+              tab-width = 2;
+              unit = "  ";
+            };
+          };
+        in
+        [
+          (lang "rust" "rustfmt")
+          (lang "c" "clang-format")
+          (lang "cpp" "clang-format")
+          (lang "yaml" "yamlfmt")
+          (lang "bash" "shfmt")
+          (lang "nix" "nixfmt")
+          (lang "python" "black")
+        ];
       language-server = {
         rust-analyzer.command = "rust-analyzer";
         clangd.command = "clangd";
+        cmake-language-server.command = "cmake-language-server";
+        qmlls.command = "qmlls";
         bash-language-server.command = "bash-language-server";
         dot-language-server.command = "dot-language-server";
         pylsp.command = "pylsp";
         nil.command = "nil";
         yaml-language-server.command = "yaml-language-server";
         docker-langserver.command = "docker-langserver";
-        cmake-language-server.command = "cmake-language-server";
-        qmlls.command = "qmlls";
       };
     };
     extraPackages = with pkgs; [
       dockerfile-language-server-nodejs
+      # c-family and related stuff
       clang-tools
-      rust-analyzer
-      nil
-      nodePackages_latest.bash-language-server
-      yaml-language-server
-      dot-language-server
-      python312Packages.python-lsp-server
+      lldb
       cmake-language-server
       kdePackages.qtdeclarative
+
+      # rust
+      rust-analyzer
+      rustfmt
+
+      # nix
+      nil
+      nixfmt-rfc-style
+
+      # shell
+      nodePackages_latest.bash-language-server
+      shellcheck
+      shfmt
+
+      # yaml
+      yaml-language-server
+      yamlfmt
+
+      dot-language-server
+
+      # python 
+      python312Packages.python-lsp-server
+      python312Packages.rope
+      python312Packages.pyflakes
+      python312Packages.pycodestyle
+      black
     ];
   };
 }
