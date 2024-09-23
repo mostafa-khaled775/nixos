b941aa8b0f40df8293b3e442df8a3aaf39da0e83
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2024-09-23 10:36:52 +0300

    remove virtualbox

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -8,14 +8,4 @@
       setSocketVariable = true;
     };
   };
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableKvm = true;
-    addNetworkInterface = false;
-    enableHardening = false;
-  };
-  virtualisation.virtualbox.guest = {
-    enable = true;
-    clipboard = true;
-  };
 }
