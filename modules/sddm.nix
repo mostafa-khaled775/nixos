8cba47c80fb8e7106491d6054ff47fc1a8b216fa
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2024-11-24 13:50:59 +0200

    change sddm's compositor to kwin

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -2,7 +2,10 @@
 {
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
+    wayland = {
+      enable = true;
+      compositor = "kwin";
+    };
     # ensures that sddm is using qt6
     package = pkgs.kdePackages.sddm;
   };
