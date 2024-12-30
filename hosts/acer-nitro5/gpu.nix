25416205df5a28f2c8975298452453ddf2c13f5b
Mostafa Khaled <mostafa.khaled.5422@gmail.com>
2024-12-30 09:24:09 +0200

    add nvidia propietry drivers specialisation

--- hosts/acer-nitro5/gpu.nix
+++ hosts/acer-nitro5/gpu.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
+  specialisation.nvidia-drivers.configuration = {
+    system.nixos.tags = [ "nvidia-drivers" ];
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+      open = false;
+      nvidiaSettings = false;
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+      prime = {
+        sync.enable = true;
+        nvidiaBusId = "PCI:01:00:0";
+        intelBusId = "PCI:00:02:0";
+      };
+    };
+  };
   hardware.graphics.enable = true;
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
--- modules/unfree.nix
+++ modules/unfree.nix
@@ -1,10 +1,16 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   nixpkgs.config.allowUnfreePredicate =
     pkg:
     builtins.elem (lib.getName pkg) (
       map lib.getName [
         pkgs.intel-ocl
+        config.hardware.nvidia.package
       ]
     );
 }
